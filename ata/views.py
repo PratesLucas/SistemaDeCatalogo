7edec7bd2d833d24c883967a044d5b1260841098
PratesLucas <lucasprates567@gmail.com>
2024-08-20 12:16:15 -0300

    Alterações no APP Atas v2

--- ata/views.py
+++ ata/views.py
@@ -1,4 +1,4 @@
-from unittest import loader
+from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from ata.forms import AtasForm
@@ -9,16 +9,14 @@ from django.contrib.auth.decorators import login_required
 def atas_view(request):
     if request.GET:
         dic = {}
-
         for chave, valor in request.GET.lists():
             dic.update({chave + "__contains": valor[0]})
-
         atas = Atas.objects.all().filter(**dic)
     else:
         atas = Atas.objects.all()
 
-    template = loader.get_template("_base.html")
-    context = {'atas' : atas}
+    template = loader.get_template('ata/atas.html')
+    context = {'atas': atas}
 
     return HttpResponse(template.render(context, request))
 
@@ -34,5 +32,8 @@ def cadastro_ata_view(request):
             return HttpResponseRedirect('/ata/')
     else:
         form = AtasForm()
+    
+    template = loader.get_template('ata/cadastro_ata.html')
+    context = {'form': form}
 
-    return render(request, 'ata/add.html', {'form' : form})
+    return HttpResponse(template.render(context, request))
--- pessoa/models.py
+++ pessoa/models.py
@@ -4,3 +4,6 @@ from accounts.models import Usuario
 class Pessoa(Usuario):
     idade = models.IntegerField()
     rg = models.CharField(max_length=14)
+
+    def __str__(self):
+        return f"Ata {self.nome} - {self.idade} - {self.rg}"
